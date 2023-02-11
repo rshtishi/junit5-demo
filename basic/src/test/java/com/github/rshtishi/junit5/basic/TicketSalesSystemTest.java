f1ae55167bf8ed5d9824644d74026da45e209612
rshtishi <rando.shtishi@gmail.com>
2023-02-11 10:44:24 +0100

    improved repeated test by adding message specifying each repetition number

--- basic/src/test/java/com/github/rshtishi/junit5/basic/TicketSalesSystemTest.java
+++ basic/src/test/java/com/github/rshtishi/junit5/basic/TicketSalesSystemTest.java
@@ -1,6 +1,9 @@
 package com.github.rshtishi.junit5.basic;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.TestInfo;
 
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,7 +11,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TicketSalesSystemTest {
 
-    @RepeatedTest(10)
+    @BeforeEach
+    void beforeEach(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        int currentRepetition = repetitionInfo.getCurrentRepetition();
+        int totalRepetitions = repetitionInfo.getTotalRepetitions();
+        String methodName = testInfo.getTestMethod().get().getName();
+        System.out.println(String.format("About to execute repetition %d of %d for %s", //
+                currentRepetition, totalRepetitions, methodName));
+    }
+
+    @RepeatedTest(value = 10, name = "{displayName} {currentRepetition}/{totalRepetitions}")
     public void testSellTicket() {
         TicketSalesSystem ticketSalesSystem = new TicketSalesSystem(100);
         for (int i = 0; i < 110; i++) {
